7f3c38aedde615453131a61e5ad322c7b662fadd
tylerpak <paktyler@utexas.edu>
2026-02-18 21:11:03 -0600

    added S3 bucket and am able to access on 9001

--- backend/src/test/java/org/yinyang/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/yinyang/backend/BackendApplicationTests.java
@@ -1,13 +1,2 @@
 package org.yinyang.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
