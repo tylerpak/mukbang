33f638e59d414560a6072d2bd72e83006a92d596
tylerpak <paktyler@utexas.edu>
2026-03-07 23:27:52 -0600

    added marker for current location

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,15 +1,43 @@
-import MapView from "react-native-maps";
+import MapView, {MapMarker} from "react-native-maps";
 import {View} from "react-native";
+import {useEffect, useState} from "react";
+import * as Location from "expo-location";
+import {Text} from "@/components/ui/text";
 
 const Map = () => {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if(status != "granted") {
+                return;
+            }
+
+            const loc = await Location.getCurrentPositionAsync({});
+            setLocation(loc);
+        })();
+    }, []);
+
+    if(!location) return(
+        <View className={'flex-1'}>
+            <Text className={"text-xl"}>Please authorize location services</Text>
+        </View>
+    )
+
     return (
         <View className={'flex-1'}>
             <MapView style={{flex: 1}} initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
-            }}/>
+            }}>
+                <MapMarker coordinate={{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude
+                }} title={'Current location'}/>
+            </MapView>
         </View>
     );
 };
