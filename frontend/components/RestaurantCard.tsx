640c8a42324c6bdba6d3a7e780380fe5a924dafe
Wyson Kong <wyson.kong.mil@army.mil>
2026-03-08 13:43:24 -0500

    restaurant card directs to the restaurant page

--- frontend/components/RestaurantCard.tsx
+++ frontend/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 import {Restaurant} from "@/util/types";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {Text} from "react-native";
+import {Text, TouchableOpacity} from "react-native";
+import {router} from "expo-router";
 
 interface RestaurantCardProps  {
     restaurant: Restaurant
@@ -8,15 +9,20 @@ interface RestaurantCardProps  {
 
 const RestaurantCard = ( {restaurant}: RestaurantCardProps) => {
 
+    const handlePress = () => {
+        router.push(`/restaurant/${restaurant.id}`);
+    };
 
     return (
             <Card className={'bg-amber-200'}>
+                <TouchableOpacity onPress={handlePress}>
                 <CardHeader>
                    <CardTitle>{restaurant.name}</CardTitle>
                 </CardHeader>
                 <CardContent>
                     <Text>Deals:</Text>
                 </CardContent>
+                </TouchableOpacity>
             </Card>
     );
 };
